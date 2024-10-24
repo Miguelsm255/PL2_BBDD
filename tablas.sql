99e26b7c3309ca420b7dabe8910a69225ed14bde
Botiquim <marcosbonilladlm@gmail.com>
2024-10-24 13:35:29 +0200

    Añadida la plantilla de creación de tablas que nos dejó el profe
    y además las tablas de las entidades, en teoría, deberían estar teminadas

--- tablas.sql
+++ tablas.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS Disco(
-    Título INT NOT NULL,
+    Título TEXT NOT NULL,
     Año_publicación INT NOT NULL,
     URL_Portada TEXT,
     CONSTRAINT Disco_pk PRIMARY KEY(Título, Año_publicación)
 );
 
 CREATE TABLE IF NOT EXISTS Canción(
-    Título INT NOT NULL,
+    Título TEXT NOT NULL,
     Duración INT,
     CONSTRAINT Canción_pk PRIMARY KEY(Título)
 );
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Géneros(
 );
 
 CREATE TABLE IF NOT EXISTS Géneros_Disco(
-    Título INT NOT NULL,
+    Título TEXT NOT NULL,
     Año_publicación INT NOT NULL,
     Nombre_Género TEXT NOT NULL,
     CONSTRAINT Géneros_Disco_pk PRIMARY KEY(Título, Año_publicación, Nombre_Género),
@@ -25,5 +25,26 @@ CREATE TABLE IF NOT EXISTS Géneros_Disco(
     CONSTRAINT fk_Géneros FOREIGN KEY(Nombre_Género) REFERENCES Géneros(Nombre_Género)
 );
 
+CREATE TABLE IF NOT EXISTS Grupo(
+    Nombre_Grupo TEXT NOT NULL,
+    URL_Imagen INT,
+    CONSTRAINT Grupo_pk PRIMARY KEY(Nombre_Grupo)
+)
 
-/*Se supone que esto está implementado correctamente, aunque aún tengo que ver cómo se hace lo de los atributos multievaluados*/
+
+CREATE TABLE IF NOT EXISTS Usuario(
+    Nombre_Usuario TEXT NOT NULL,
+    Nombre TEXT NOT NULL,
+    Contraseña TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    CONSTRAINT Usuario_pk PRIMARY KEY(Nombre_Usuario)
+)
+
+CREATE TABLE IF NOT EXISTS Ediciones(
+    Formato TEXT NOT NULL,
+    País TEXT NOT NULL,
+    Año_publicación INT NOT NULL,
+    CONSTRAINT 
+
+)
+
